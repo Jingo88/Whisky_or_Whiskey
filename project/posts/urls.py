9a1da1963088d52323d29f62d3ef4c5c7e4e913c
Jason <Jcory.ng@gmail.com>
2016-01-31 20:06:46 -0500

    class based views, list okay to go, create/detail view are up next

--- project/posts/urls.py
+++ project/posts/urls.py
@@ -1,16 +1,38 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+###########################################################################
+##########	URL Patters Class Based Views		###############
+###########################################################################
 from .views import(
-	posts_list,
-	posts_create,
-	posts_detail,
-	posts_update,
+	Posts_List,
+	Posts_Create,
+	Posts_Detail,
+	# Posts_Update,
 	)
 
 urlpatterns = [
-	url(r'^$', posts_list, name='list'),
-	url(r'^create$', posts_create, name='create'),
-	url(r'^(?P<id>\d+)/$', posts_detail, name='detail'),
-	url(r'^(?P<id>\d+)/update$', posts_update, name='update'),
+	url(r'^$', Posts_List.as_view(), name='list'),
+	url(r'^create$', Posts_Create.as_view(), name='create'),
+	url(r'^(?P<id>\d+)/$', Posts_Detail.as_view(), name='detail'),
+	# url(r'^(?P<id>\d+)/update$', Posts_Update.as_view(), name='update'),	
 ]
+
+
+###########################################################################
+##########	URL Patterns Function Based Views		###############
+###########################################################################
+
+# from .views import(
+# 	posts_list,
+# 	posts_create,
+# 	posts_detail,
+# 	posts_update,
+# 	)
+
+# urlpatterns = [
+# 	url(r'^$', posts_list, name='list'),
+# 	url(r'^create$', posts_create, name='create'),
+# 	url(r'^(?P<id>\d+)/$', posts_detail, name='detail'),
+# 	url(r'^(?P<id>\d+)/update$', posts_update, name='update'),
+# ]
--- project/posts/views.py
+++ project/posts/views.py
@@ -2,61 +2,112 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django import forms
 from .models import Post
 from posts.forms import PostForm
+from django.views.generic import View
 
-# Create your views here.
+###########################################################################
+##########	WE ARE CREATING CLASS BASED VIEWS BELOW		###############
+###########################################################################
 
-def posts_list(req):
-	whiskey_list = Post.objects.all()
-	context = {
-		"whiskey_list" : whiskey_list,
-	}
-	return render(req, "posts/list.html", context)
+class Posts_List(View):
+	def get(self, req):
+		whiskey_list = Post.objects.all()
+		context = {
+			"whiskey_list": whiskey_list,
+		}
+		return render(req, "posts/list.html", context)
 
-def posts_create(req):
-	create_form = PostForm(req.POST or None)
+class Posts_Create(View):
+	form_class = PostForm
+	initial = {'key': 'value'}
+	create_template = 'posts/create.html'
 
-	if create_form.is_valid():
-		instance = create_form.save(commit=False)
-		instance.save()
-		# return HttpReponseRedirect(instance.get_absolute_url())
-		return redirect(instance)
-	context = {
-		"create":create_form,
-	}
-	return render(req, "posts/create.html", context)
+	def get(self, req, *args, **kwargs):
+		create_form = self.form_class(initial=self.initial)
+		context = {
+			"create": create_form
+		}
+		return render(req, self.create_template, context)
 
-def posts_detail(req, id=None):
-	whiskey = get_object_or_404(Post, id=id)
-	print(whiskey.id)
-	context = {
-		"whiskey": whiskey
-	}
-	return render(req, "posts/detail.html", context)
+	def post(self, req, *args, **kwargs):
+		create_form = self.form_class(req.POST or None)
+		print(create_form)
+		if create_form.is_valid():
+			instance = create_form.save(commit=False)
+			instance.save()
+			return redirect(instance)
+		context = {
+			"create":create_form,
+		}
+		return render(req, "posts/list.html", context)
 
-def posts_update(req, id=None):
+class Posts_Detail(View):
+	detail_template = 'posts/detail.html'
 
-	whiskey = get_object_or_404(Post, id=id)
+	def get_context_data(self, **kwargs):
+		context = super(Posts_Detail, self).get_context_data(**kwargs)
+		context['posts'] = Post.objects.get()
+		return context
 
-# "initial" is a attribute of a form set?
-	# update_form = PostForm(initial = {
-	# 	"brand": whiskey.brand, 
-	# 	"brand_type": whiskey.brand_type,
-	# 	"price" : whiskey.price,
-	# 	"description": whiskey.description
-	# 	})
-# instance = whiskey is something learned from Jeff
-	update_form = PostForm(req.POST or None, instance = whiskey)
-	print(update_form)
-	if update_form.is_valid():
-		instance = update_form.save()
-		# instance.save()
-		return redirect(instance)
-	context = {
-		"whiskey": update_form,
-	}
+	def get(self, req):	
+		return render(req, "posts/list.html", context)
 
-	# PostForm(data=request.POST)
-	return render(req, "posts/update.html", context)
+
+###########################################################################
+##########	WE ARE CREATING FUNCTION BASED VIEWS BELOW		###############
+###########################################################################
+
+# def posts_list(req):
+# 	whiskey_list = Post.objects.all()
+# 	context = {
+# 		"whiskey_list" : whiskey_list,
+# 	}
+# 	return render(req, "posts/list.html", context)
+
+# def posts_create(req):
+# 	create_form = PostForm(req.POST or None)
+
+# 	if create_form.is_valid():
+# 		instance = create_form.save(commit=False)
+# 		instance.save()
+# 		# return HttpReponseRedirect(instance.get_absolute_url())
+# 		return redirect(instance)
+# 	context = {
+# 		"create":create_form,
+# 	}
+# 	return render(req, "posts/create.html", context)
+
+# def posts_detail(req, id=None):
+# 	whiskey = get_object_or_404(Post, id=id)
+# 	print(whiskey.id)
+# 	context = {
+# 		"whiskey": whiskey
+# 	}
+# 	return render(req, "posts/detail.html", context)
+
+# def posts_update(req, id=None):
+
+# 	whiskey = get_object_or_404(Post, id=id)
+
+# # "initial" is a attribute of a form set?
+# 	# update_form = PostForm(initial = {
+# 	# 	"brand": whiskey.brand, 
+# 	# 	"brand_type": whiskey.brand_type,
+# 	# 	"price" : whiskey.price,
+# 	# 	"description": whiskey.description
+# 	# 	})
+# # instance = whiskey is something learned from Jeff
+# 	update_form = PostForm(req.POST or None, instance = whiskey)
+# 	print(update_form)
+# 	if update_form.is_valid():
+# 		instance = update_form.save()
+# 		# instance.save()
+# 		return redirect(instance)
+# 	context = {
+# 		"whiskey": update_form,
+# 	}
+
+# 	# PostForm(data=request.POST)
+# 	return render(req, "posts/update.html", context)
 
 
 
